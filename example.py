647fd75a42e16e247cbcb26a75fb86a27c5d5d32
nv-h <saido.nv@gmail.com>
2020-11-19 09:50:53 +0000

    example.py: i2c bus error

--- example.py
+++ example.py
@@ -15,5 +15,8 @@ while(True):
         voc, co2 = ccs811.get()
         print(f"{p:7.2f} hPa, {t:6.2f} C, {h:5.2f} %, TVOC:{voc:4d} ppb, eCO2:{co2:4d} ppm")
         sleep(1)
+    except OSError:
+        # i2c bus somtimes cannot access
+        continue
     except KeyboardInterrupt:
         break
