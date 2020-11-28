a453559ff7319085c71320fae6b089819425b746
H.Saido <saido.nv@gmail.com>
2020-11-28 17:55:51 +0900

    Display forecast in Dash

--- openweathermap/get_weather_data.py
+++ openweathermap/get_weather_data.py
@@ -1,8 +1,16 @@
 #!/bin/env python
 
-import matplotlib.pyplot as plt
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
 import pandas as pd
 import numpy as np
+
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+from datetime import datetime, timedelta
+
+import os
 
 from api_key import API_KEY
 import urllib.request
@@ -34,7 +42,56 @@ response_body = get_response_body(FORECAST_URL)
 # print(json.dumps(response_body, indent=4))
 
 df = pd.json_normalize(response_body['list'])
-df.plot(x='dt_txt', y=['main.temp', 'main.humidity'])
-df.plot(x='dt_txt', y=['main.pressure'])
+df = df.set_index('dt_txt')
+df.index = pd.to_datetime(df.index)
+df.index = df.index.tz_localize('UTC')
+df.index = df.index.tz_convert('Asia/Tokyo')
 
-plt.show()
+fig = go.Figure()
+fig.add_trace(
+    go.Scatter(x=df.index, y=df['main.pressure'], name='Pressure hPa', yaxis="y1",),
+)
+fig.add_trace(
+    go.Scatter(x=df.index, y=df['main.humidity'], name='Humidity %', yaxis="y2",),
+)
+fig.add_trace(
+    go.Scatter(x=df.index, y=df['main.temp'], name='Celsius', yaxis="y3",),
+)
+
+fig.update_layout(
+    legend=dict(
+        orientation="h",
+        yanchor="bottom", xanchor="left",
+        x=0, y=1.06,
+    ),
+    xaxis=dict(
+        rangeslider=dict(
+            autorange=True,
+        ),
+        type = "date",
+        side = 'top',
+    ),
+    yaxis1=dict(domain=[0.67, 1.00], side="right", title="hPa"),
+    yaxis2=dict(domain=[0.34, 0.66], side="right", title="%"),
+    yaxis3=dict(domain=[0.00, 0.33], side="right", title="C"),
+)
+fig.update_layout(
+    dragmode="zoom",
+    hovermode="x",
+    height=800,
+)
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div(
+    children=[
+        dcc.Graph(
+            id='graph', figure=fig,
+            responsive='auto',
+        ),
+    ], 
+)
+
+if __name__ == '__main__':
+    app.run_server(debug=True, host=os.uname()[1], port='5001')
