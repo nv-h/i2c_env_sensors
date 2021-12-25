e7ee038ae9bdb897ea889c6204afe2d003243913
nv-h <saido.nv@gmail.com>
2021-12-25 17:54:54 +0900

    Fixed warnings of new version of dash

--- dash_from_csv.py
+++ dash_from_csv.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc
+from dash import html
 import pandas as pd
 
 import plotly.express as px
