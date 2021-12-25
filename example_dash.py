1fd3183186024cc0c454f1894ead79f21f62ae72
nv-h <saido.nv@gmail.com>
2021-12-25 10:21:06 +0000

    Fixed env name (python to python3)

--- example_dash.py
+++ example_dash.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import dash
 import dash_core_components as dcc
--- example_gui.py
+++ example_gui.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from bme280 import BME280
 from ccs811 import CCS811
